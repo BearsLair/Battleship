5ce17b9acc0e731c764bad13408b23bb0ada8f0f
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-12-18 21:20:37 -0800

    Feat: basic ship class

---
+++ ships.js
@@ -0,0 +1,18 @@
+/*
+Carrier: 5
+Battleship: 4
+Cruiser: 3
+Submarine: 3
+Destroyer: 2
+*/
+
+class ship {
+  constructor(type, length, hitPoints) {
+    this.type = type;
+    this.length = length;
+    this.hitPoints = hitPoints;
+    this.isSunk = false;
+  }
+}
+
+const createFleet = () => {};
