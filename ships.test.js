24b1c06bfa23edca171f626eb3cea1370fac1f7a
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-12-19 02:17:50 -0800

    Updated tests to make sure objects have all properties

--- ships.test.js
+++ ships.test.js
@@ -1,11 +1,28 @@
 import createFleet from "./ships";
 
-test("Create ships", () => {
-  expect(createFleet()).toEqual([
-    { type: Carrier, length: 5, hitPoints: 5, isSunk: false },
-    { type: Battleship, length: 4, hitPoints: 4, isSunk: false },
-    { type: Cruiser, length: 3, hitPoints: 3, isSunk: false },
-    { type: Submarine, length: 3, hitPoints: 3, isSunk: false },
-    { type: Destroyer, length: 2, hitPoints: 2, isSunk: false },
-  ]);
+test("Each ship object has a type", () => {
+  const data = createFleet();
+  expect(data.every((item) => typeof item.type === "string")).toBe(true);
+});
+test("Each ship object has a length", () => {
+  const data = createFleet();
+  expect(data.every((item) => typeof item.length === "number")).toBe(true);
+});
+test("Each ship object has hitPoints", () => {
+  const data = createFleet();
+  expect(data.every((item) => typeof item.hitPoints === "number")).toBe(true);
+});
+test("Each ship object has a isSunk property", () => {
+  const data = createFleet();
+  expect(data.every((item) => typeof item.isSunk === "boolean")).toBe(true);
+});
+test("Each ship object has a hit method", () => {
+  const data = createFleet();
+  expect(data.every((item) => typeof item.hit === "function")).toBe(true);
+});
+test("Each ship object has a sunk method", () => {
+  const data = createFleet();
+  expect(data.every((item) => typeof item.isSunkFunc === "function")).toBe(
+    true
+  );
 });
