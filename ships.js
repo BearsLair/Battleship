b2e1641d55abe52028aac8c1b800236ed61efaf7
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-12-18 21:59:50 -0800

    Added hit and isSunk methods to ship class

--- ships.js
+++ ships.js
@@ -1,11 +1,3 @@
-/*
-Carrier: 5
-Battleship: 4
-Cruiser: 3
-Submarine: 3
-Destroyer: 2
-*/
-
 class ship {
   constructor(type, length, hitPoints) {
     this.type = type;
@@ -13,6 +5,14 @@ class ship {
     this.hitPoints = hitPoints;
     this.isSunk = false;
   }
+  hit = () => {
+    this.hitPoints - 1;
+  };
+  isSunk = () => {
+    if (this.hitPoints === 0) {
+      this.isSunk = true;
+    }
+  };
 }
 
 const shipTypes = [
