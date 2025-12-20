887f988595cbdead8e5b725c2e25303be4406418
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-12-19 16:56:50 -0800

    createGameboard passed tests

--- gameboard.js
+++ gameboard.js
@@ -13,7 +13,9 @@ class Cell {
   }
 }
 
-class Gameboards {
+// Type in Gameboard class refers to "Ship" placement board,
+// or "Strategy" board for recording hits and misses on opponent.
+class Gameboard {
   constructor(player, type) {
     this.player = player;
     this.type = type;
@@ -22,6 +24,23 @@ class Gameboards {
   }
 }
 
-const createBoard = (player, type) => {};
+const createBoard = (player, type) => {
+  let newBoard = new Gameboard(player, type);
+
+  const charArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+  const numArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  let id = "";
+  let newCell;
+
+  for (let i = 0; i < 10; i++) {
+    for (let k = 0; k < 10; k++) {
+      id = charArray[i] + numArray[k];
+      newCell = new Cell(id);
+      newBoard.board.push(newCell);
+    }
+  }
+
+  return newBoard;
+};
 
 export default createBoard;
