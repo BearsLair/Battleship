9ef532928bcfe28bd345345766fedf5669606773
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-12-23 20:56:45 -0800

    Player class created

---
+++ player.js
@@ -0,0 +1,10 @@
+class Player {
+  constructor(name, humanOrCPU) {
+    this.name = name;
+    this.humanOrCPU = humanOrCPU;
+    this.shipBoard = [];
+    this.strategyBoard = [];
+  }
+}
+
+export default Player;
