b9501afe466a413a33ad75192ceeabe47de3aa8d
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-12-23 18:02:32 -0800

    Fixed stupid typo

--- gameboard.test.js
+++ gameboard.test.js
@@ -12,6 +12,24 @@ const checkArrayForUniques = (array) => {
   }
 };
 
+// Check if exact array exists in parent array
+const arrayPresent = (parentArray, targetArray) => {
+  if (parentArray.length === 0) {
+    return false;
+  }
+
+  const x = targetArray[0];
+  const y = targetArray[1];
+
+  for (let i = 0; i < parentArray.length; i++) {
+    if (parentArray[i][0] === x && parentArray[i][1] === y) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
 test("Gameboard has a player", () => {
   const newBoard = createBoard("Player1", "Ship");
   expect(newBoard.player).toBe("Player1");
@@ -38,10 +56,10 @@ test("Each cell has a unique id", () => {
   expect(checkArrayForUniques(ids)).toBe(true);
 });
 
-test("A cell has a hit condition", () => {
+test("A cell has a miss condition", () => {
   const newBoard = createBoard("Player1", "Ship");
 
-  expect(newBoard.board[0].hit).toBe(false);
+  expect(newBoard.board[0].miss).toBe(false);
 });
 
 test("The board has a shipPositions array", () => {
@@ -62,6 +80,7 @@ test("shipPositions array has length of five", () => {
   newBoard.addShips();
   expect(newBoard.shipPositions).toHaveLength(5);
 });
+
 test("The ships are carrier, battleship, cruiser, submarine, and destoyer", () => {
   const newBoard = createBoard("Player1", "Ship");
   newBoard.addShips();
@@ -93,4 +112,56 @@ test("Battleship at B5 in vertical position", () => {
     [1, 6],
     [1, 7],
   ]);
+});
+
+test("arrayPresent returns correctly", () => {
+  const newBoard = createBoard("Player1", "Ship");
+  expect(
+    newBoard.arrayPresent(
+      [
+        [2, 3],
+        [3, 3],
+        [5, 6],
+      ],
+      [3, 3]
+    )
+  ).toBe(true);
+  expect(
+    newBoard.arrayPresent(
+      [
+        [2, 3],
+        [3, 3],
+        [5, 6],
+      ],
+      [1, 4]
+    )
+  ).toBe(false);
+});
+
+test("Ship positions array on the board is not empty", () => {
+  const newBoard = createBoard("Player1", "Ship");
+  newBoard.addShips();
+  expect(newBoard.shipPositions.length).not.toBe(0);
+});
+
+test("Received an attack that missed a ship", () => {
+  const newBoard = createBoard("Player1", "Ship");
+  newBoard.addShips();
+  newBoard.receiveAttack([0, 0]);
+  expect(newBoard.board[0].miss).toBe(true);
+});
+
+test("Carrier received an attack that reduced hitpoints to 4", () => {
+  const newBoard = createBoard("Player1", "Ship");
+  newBoard.addShips();
+  newBoard.receiveAttack([3, 0]);
+  console.log(newBoard.shipPositions[0].hitPoints);
+  expect(newBoard.shipPositions[0]).toMatchObject({ hitPoints: 4 });
+});
+
+test("Carrier received an attack on coordinate [3,0]", () => {
+  const newBoard = createBoard("Player1", "Ship");
+  newBoard.addShips();
+  newBoard.receiveAttack([3, 0]);
+  expect(newBoard.shipPositions[0].hitCoordinates[0]).toStrictEqual([3, 0]);
 });
