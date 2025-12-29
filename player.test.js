f9496556a5e074e87083decfe5784eb06069bd6a
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-12-28 16:04:53 -0800

    added test for player class

--- player.test.js
+++ player.test.js
@@ -1,1 +1,15 @@
 import Player from "./player";
+
+test("A player has a name, type, ship and strategy boards", () => {});
+
+test("PLayer 1 shipBoard has 100 cells", () => {});
+
+test("Player 1 strategyBoard has 100 cells", () => {});
+
+test("Player 1 is a human opponent", () => {});
+
+test("Player 1 has his five ships set on shipboard correctly");
+
+test("Player 2 is a CPU opponent", () => {});
+
+test("PLayer 2 has his ships set on the shipboard correctly");
